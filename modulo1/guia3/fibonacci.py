66ab95d2fbf7f69ff7c1893b1a3f28e1324d1584
jesus andres zini <Jesus.Zini@hstpathways.com>
2024-05-16 10:43:22 -0300

    add fibonacci

--- modulo1/guia3/fibonacci.py
+++ modulo1/guia3/fibonacci.py
@@ -1,4 +1,25 @@
+#
+# definimos variable global y la funcion
+#
+nun_llamadas = 0
+
 
 def fibonacci(n):
-    """  TODO   """
-    pass        
+    global nun_llamadas  # para poder modificar la variable global
+    nun_llamadas += 1
+
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+#
+# ejecutamos la funcion
+#
+n = input("Ingrese n-ésimo término en la secuencia quiera calcular: ")
+
+res = fibonacci(n)
+
+print("secuencia:", res)
+print(f"numero de llamadas: {nun_llamadas}")
--- modulo1/guia3/tendencia_central.py
+++ modulo1/guia3/tendencia_central.py
@@ -1,15 +1,38 @@
+#
+# definimos la funcion
+#
+def tendencia_central(lista, tipo="media"):
+    """
+    Calcula la tendencia central de una lista de numeros.
+    """
 
-def tendencia_central(lista,tipo='media'):
-    """  TODO   """
-    if   tipo=='media':
-        #TODO
-        pass
-    elif tipo=='mediana':
-        #TODO
-        pass
-    elif tipo=='moda':
-        #TODO
-        pass
+    if tipo == "media":
+        return sum(lista) / len(lista)
+
+    elif tipo == "mediana":
+        sorted_list = sorted(lista)
+        n = len(sorted_list)
+        if n % 2 == 0:
+            return (sorted_list[n // 2 - 1] + sorted_list[n // 2]) / 2
+        else:
+            return sorted_list[n // 2]
+
+    elif tipo == "moda":
+        counts = {x: lista.count(x) for x in set(lista)}
+        max_count = max(counts.values())
+        return [key for key, value in counts.items() if value == max_count]
+
     else:
-        #TODO
-        pass        
+        return "tipo desconocido"
+
+
+#
+# ejecutamos la funcion
+#
+# [8, 2, 3, 8, 0, 7, 5, 1, 2, 3]
+#
+lista = input("Ingrese una lista: ")
+metodo = input("Elija un metodo (media, mediana o moda): ")
+
+res = tendencia_central(lista, metodo)
+print(res)
