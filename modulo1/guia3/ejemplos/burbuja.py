30b2a8131734bca8922b145d4760068051a58c0b
jesusandres31 <jesusandreszini14@gmail.com>
2024-05-16 00:46:19 -0300

    factorial done

--- modulo1/guia3/ejemplos/burbuja.py
+++ modulo1/guia3/ejemplos/burbuja.py
@@ -1,5 +1,32 @@
-
+#
+# definimos la funcion
+#
 def burbuja(seq):
-    """ TODO """
-    
-    # TODO
+    n = len(seq)
+
+    for i in range(n):
+        intercambio_realizado = False
+
+        for j in range(0, n - i - 1):
+            if seq[j] > seq[j + 1]:
+                seq[j], seq[j + 1] = seq[j + 1], seq[j]
+                intercambio_realizado = True
+
+        if not intercambio_realizado:
+            break
+
+    return seq
+
+
+#
+# ejecutamos la funcion
+#
+""" numero = input("Ingrese un numero: ")
+metodo = input("Elija un metodo (iterativo o recursivo): ")
+
+res = factorial(numero, metodo)
+print(res) """
+
+lista = [8, 2, 3, 8, 0, 7, 5, 1, 2, 3]
+print("Lista original:", lista)
+print("Lista ordenada:", ordenamiento_burbuja(lista))
--- modulo1/guia3/ejemplos/factorial.py
+++ modulo1/guia3/ejemplos/factorial.py
@@ -1,12 +1,38 @@
+#
+# definimos la funcion
+#
+def factorial(n, metodo="iterativo"):
+    n = int(n)
 
-def factorial(lista,metodo='iterativo'):
-    """  TODO   """
-    if   metodo=='iterativo':
-        #TODO
-        pass
-    elif tipo=='recursivo':
-        #TODO
-        pass
+    if metodo == "iterativo":
+        # usamos un bucle "for"
+        res = 1
+
+        for i in range(1, n + 1):
+            res = res * i
+
+        return res
+
+    elif metodo == "recursivo":
+        print(f"ahora n vale: {n}")
+        # usamos funcion recursiva
+        if n < 0:
+            return "Error. Ingrese un numero positivo"
+        elif n == 0:
+            return 1
+        else:
+            res = n * factorial(n - 1, metodo)
+            return res
+
     else:
-        #TODO
-        pass        
+        return "metodo desconocido"
+
+
+#
+# ejecutamos la funcion
+#
+numero = input("Ingrese un numero: ")
+metodo = input("Elija un metodo (iterativo o recursivo): ")
+
+res = factorial(numero, metodo)
+print(f"resultado: {res}")
