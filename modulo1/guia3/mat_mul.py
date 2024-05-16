65805d3025f16fbe70f3702eda4ae9ca7a04ddbe
jesus andres zini <Jesus.Zini@hstpathways.com>
2024-05-16 17:38:26 -0300

    add mat_mul

--- modulo1/guia3/mat_mul.py
+++ modulo1/guia3/mat_mul.py
@@ -1,41 +1,40 @@
-def mat_mul(A, B):
-    """
-    Calcula el producto de dos matrices A y B.
+#
+# definimos la funcion
+#
+def obtener_dimension_matriz(matriz):
+    long_fila = len(matriz)
+    long_columna = len(matriz[0])  # asumimos que pasamos todas las col de la misma long
+    return [long_fila, long_columna]
 
-    Argumentos:
-    A (list of lists): Matriz A
-    B (list of lists): Matriz B
 
-    Retorna:
-    list of lists: Matriz resultante del producto A * B
-    """
-    # Verificar dimensiones compatibles
-    if len(A[0]) != len(B):
-        raise ValueError(
-            "Las dimensiones de las matrices no son compatibles para la multiplicación."
-        )
+def mat_mul(a, b):
+    dim_a = obtener_dimension_matriz(a)
+    dim_b = obtener_dimension_matriz(b)
 
-    # Inicializar matriz resultante
-    result = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    if dim_a[1] != dim_b[0]:
+        return "tus matrices no tiene dimensiones compatibles"
 
-    # Calcular producto de matrices
-    for i in range(len(A)):
-        for j in range(len(B[0])):
-            for k in range(len(B)):
-                result[i][j] += A[i][k] * B[k][j]
+    # inicializar matriz resultante
+    result = []
+
+    # multiplicar matrices
+    for i in range(dim_a[0]):
+        row_result = []
+        for j in range(dim_b[1]):
+            sum = 0
+            for k in range(dim_a[1]):  # dim_a[1] debe ser igual a dim_b[0]
+                sum += a[i][k] * b[k][j]
+            row_result.append(sum)
+        result.append(row_result)
 
     return result
 
 
-# Testear la función
-if _name_ == "_main_":
-    A = [[1, 2, 3], [4, 5, 6]]
-    B = [[7, 8], [9, 10], [11, 12]]
+#
+# ejecutamos la funcion
+#
+a = input("ingrese la matriz a:")
+b = input("ingrese la matriz b:")
 
-    try:
-        result = mat_mul(A, B)
-        print("El producto de las matrices es:")
-        for row in result:
-            print(row)
-    except ValueError as e:
-        print(e)
+res = mat_mul(a, b)
+print(res)
